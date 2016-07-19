c9cc0e202faa13e3ebc6464fde62f2cef749ca1b
Derrick Yoo <dky@email.arizona.edu>
2016-07-19 16:38:07 -0700

    added get full path name function to journal

--- apps/04_journal/journal.py
+++ apps/04_journal/journal.py
@@ -3,16 +3,22 @@ import os
 
 def load(name):
     # todo: populate from file if it exists.
+    filename = get_full_pathname(name)
     return []
 
 
 def save(name, journal_data):
-    filename = os.path.abspath(os.path.join('.', 'journals' + name + '.jrl'))
+    filename = get_full_pathname(name)
 
     with open(filename, 'w') as fout:
 
         for entry in journal_data:
             fout.write(entry + '\n')
 
+
 def add_entry(text, journal_data):
     journal_data.append(text)
+
+
+def get_full_pathname(name):
+    filename = os.path.abspath(os.path.join('.', 'journals' + name + '.jrl'))
