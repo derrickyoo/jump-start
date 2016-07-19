31dbf7745395da81d63fdfc963555a912afa4d10
Derrick Yoo <dky@email.arizona.edu>
2016-07-19 14:18:21 -0700

    Added guess number program app

---
+++ apps/02_guess_number/program.py
@@ -0,0 +1,20 @@
+import random
+
+print('--------------------------------------------')
+print('           GUESS THAT NUMBER GAME           ')
+print('--------------------------------------------')
+print()
+
+the_num = random.randint(0, 100)
+guess_num = -1
+
+while guess_num != the_num:
+    guess_str = input('Guess a number between a 0 and 100: ')
+    guess_num = int(guess_str)
+
+    if guess_num < the_num:
+        print('Sorry but {} is LOWER than the number'.format(guess_str))
+    elif guess_num > the_num:
+        print('Sorry but {} is HIGHER than the number'.format(guess_str))
+    else:
+        print('YES! You\'ve got it. The number was {}'.format(guess_str))
