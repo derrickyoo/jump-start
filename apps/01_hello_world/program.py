185126817c624cae4f4d8c2163d5a4659b2e0db9
Derrick Yoo <dky@email.arizona.edu>
2016-07-19 13:54:20 -0700

    Added app directory and first app

---
+++ apps/01_hello_world/program.py
@@ -0,0 +1,8 @@
+print('-------------------------------')
+print('           HELLO APP           ')
+print('-------------------------------')
+print()
+
+name = input('What is your name? ')
+output = 'Nice to meet you {}'.format(name)
+print(output)
