37269f1fd9b53b21f4027cf8339ca3b1c7963cca
derrickyoo <derrickyoo@gmail.com>
2016-08-21 22:05:54 -0700

    Updated weather app with next step using beautifulsoup4

--- apps/05_weather_client/program.py
+++ apps/05_weather_client/program.py
@@ -1,5 +1,35 @@
+import requests
+
+
 def main():
+    print_header()
+    zip_code = input('Enter ZIP code: ')
+    html = get_html_weather_from_web(zip_code)
+    # parse the html
+    # display forcast
+
+
+def print_header():
+    print('------------------------------------------')
+    print('           Weather Client App             ')
+    print('------------------------------------------')
+    print()
+
+
+def get_html_weather_from_web(zip_code):
+    url = 'https://www.wunderground.com/weather-forecast/{}'.format(zip_code)
+    response = requests.get(url)
+
+    return response
+
+
+def parse_html_from_web():
     pass
+
+
+def display_forcast():
+    pass
+
 
 if __name__ == "__main__":
     main()
