42352b3e9a5f17988d509a72c7cc35b3a06d505e
Derrick Yoo <dky@email.arizona.edu>
2016-08-22 15:17:01 -0700

    Added base cat_factory app

---
+++ apps/06_cat_factory/program.py
@@ -0,0 +1,15 @@
+def main():
+    print_header()
+    # get or create output folder
+    # download cats
+    # display cats
+
+
+def print_header():
+    print('------------------------------------------')
+    print('           Cat Fatory App                 ')
+    print('------------------------------------------')
+    print()
+
+if __name__ == "__main__":
+    main()
