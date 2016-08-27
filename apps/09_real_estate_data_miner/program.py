dff01faf9b8c7fd50cff2d38941d271f75e492ab
derrickyoo <derrickyoo@gmail.com>
2016-08-27 16:52:34 -0700

    Added base app 9 data miner app

---
+++ apps/09_real_estate_data_miner/program.py
@@ -0,0 +1,13 @@
+def main():
+    print_header()
+
+
+def print_header():
+    print('--------------------------------------------')
+    print('       Real Estate Data Miner App           ')
+    print('--------------------------------------------')
+    print()
+
+
+if __name__ == '__main__':
+    main()
